72a670839f5a0bcd1a01e09bc4183f79637ab369
Felipe Stawinski Suzuki <fsuzuki@alunos.utfpr.edu.br>
2025-08-20 18:48:28 -0300

    main page enhancement

--- kpi-master-front/src/app/(app)/main/page.tsx
+++ kpi-master-front/src/app/(app)/main/page.tsx
@@ -2,6 +2,10 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { HiMagnifyingGlass } from "react-icons/hi2";
+import { CgFileDocument } from "react-icons/cg";
+import { FcStatistics } from "react-icons/fc";
 
 export default function MainPage() {
   const router = useRouter();
@@ -41,13 +45,42 @@ export default function MainPage() {
   };
 
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen p-11">
 
-      {/* Main content */}
-      <div className="flex-1 p-8 bg-gray-100 flex items-center flex-col justify-center">
-        <h1 className="text-4xl font-semibold text-gray-600 ">Hello,</h1>
-        <h1 className="text-5xl font-semibold text-amber-500">{username}</h1>
-        <p className="text-3xl mt-4 text-gray-700">Welcome to your dashboard.</p>
+      {/* Welcome message */}
+      <div className="flex-1 bg-gray-100 flex flex-col ">
+        <div className='flex-initial'>
+          <h1 className="text-4xl font-semibold text-gray-600 ">Hello,</h1>
+          <h1 className="text-5xl font-semibold text-amber-500">{username}</h1>
+          <p className="text-3xl mt-4 text-gray-700">Welcome to your dashboard.</p>
+        </div>
+
+        {/* Introduction */}
+        <div className='flex-1 flex items-center flex-col justify-center space-y-4'>
+        <p className="text-gray-600">Aqui você pode...</p>
+          <div className='flex flex-row space-x-10'>  
+              <div className='flex flex-col'>
+                <button className='bg-amber-500 p-8 hover:bg-amber-700 rounded text-gray-800 flex justify-center items-center flex-col'>
+                  Pesquisar documentos
+                  <HiMagnifyingGlass size={30} color='black'/>
+                </button>
+  
+              </div>
+              <div className='flex flex-col'>
+                <button className='bg-amber-500 p-8 hover:bg-amber-700 rounded text-gray-800 flex justify-center items-center flex-col'>
+                  Carregar documentos
+                  <AiOutlineCloudUpload size={30} color='black'/>
+                </button>
+              </div>
+              <div className='flex flex-col'>
+                <button className='bg-amber-500 p-8 hover:bg-amber-700 rounded text-gray-800 flex justify-center items-center flex-col'>
+                  Gerar estatísticas
+                  <FcStatistics size={30} color='black' />
+                </button>
+              </div>
+            
+          </div>
+        </div>
       </div>
     </div>
   );
