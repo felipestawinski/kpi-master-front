ff36c7a608f84f4dfb9d50c293b51aa48b70dfa5
Felipe Stawinski Suzuki <felipe.stawinski@gmail.com>
2025-10-20 23:29:38 -0300

    lobby page

--- kpi-master-front/src/app/(public)/lobby/page.tsx
+++ kpi-master-front/src/app/(public)/lobby/page.tsx
@@ -1,12 +1,11 @@
+import Header from '../../../components/Header';
+import About from '../../../components/About';
+
 export default function LobbyPage() {
   return (
-    <div className="min-h-screen bg-[url('/background3.jpg')] bg-cover bg-center bg-no-repeat">
-      {/* Your content with overlay */}
-      <div className="min-h-screen bg-black/20 flex items-center justify-center">
-        <div className="text-white text-center">
-          <h1 className="text-4xl font-bold">Welcome</h1>
-        </div>
-      </div>
-    </div>
+    <main className="min-h-screen m-0 p-0">
+      <Header />
+      <About />
+    </main>
   );
 }
---
+++ kpi-master-front/src/components/Header.tsx
@@ -0,0 +1,19 @@
+'use client';
+import Silk from './Silk';
+
+export default function Header () {
+    return (
+        <header className="!h-[40vh] flex flex-col items-center justify-center bg-gray-800 text-white relative overflow-hidden">
+            <div className='absolute inset-0 z-0'>
+                <Silk
+                speed={5}
+                scale={1}
+                color="#7B7481"
+                noiseIntensity={1.5}
+                rotation={0}
+                /> 
+            </div>
+            <h1 className="text-2xl font-bold z-10">Welcome to the KPI project</h1>
+        </header>
+    )
+}
